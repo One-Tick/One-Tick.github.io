1b7711e16647a305a78cd1590418b9bd8cb2dfc2
michbur <michalburdukiewicz@gmail.com>
2025-03-26 14:36:38 +0100

    updated the webpage

--- render-deploy.R
+++ render-deploy.R
@@ -2,4 +2,5 @@ system("quarto render")
 #unlink("./docs/7367971090b4eeba727ec18588031821135ed2ea", recursive = TRUE)
 #file.rename(from = "./_site/", to = "./docs/7367971090b4eeba727ec18588031821135ed2ea/")
 
+unlink("./docs/", recursive = TRUE)
 file.rename(from = "./_site/", to = "./docs/")
